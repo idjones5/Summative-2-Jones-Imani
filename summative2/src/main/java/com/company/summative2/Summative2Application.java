69a2c8920ac071440421318680c771e4fd82a78b
idjones5 <97227301+idjones5@users.noreply.github.com>
2022-10-27 16:48:08 -0700

    Initial Commit
    Project structure.

---
+++ summative2/src/main/java/com/company/summative2/Summative2Application.java
@@ -0,0 +1,13 @@
+package com.company.summative2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative2Application.class, args);
+	}
+
+}
---
+++ summative2/src/test/java/com/company/summative2/Summative2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.summative2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
